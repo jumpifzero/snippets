cb469ff464a027e6d12645c9a44ee01a44c8dae6
Tiago Almeida <tiago.b.almeida@gmail.com>
2017-10-07 15:08:12 +0100

    Generates passwords with non-representable characters

--- experiments/elm/prog1.elm
+++ experiments/elm/prog1.elm
@@ -1,49 +1,93 @@
--- Read more about this program in the official Elm guide:
--- There is a problem with this program, try to figure out what it is
-
-
-import Html exposing (beginnerProgram, div, button, text, input)
+-- A simple password generator to get a feel for ELM
+    
+import Html exposing (program, div, button, text, input)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (..)
+import Random
+import Random.Char
+import Debug
 
+
+main =
+  program 
+  { 
+    init = init 
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+    }
+    
+
+
+type Msg 
+  = Generate
+  | LengthChange String
+  | NewRandomChar Char
+
+
+-- ======================================================================
 -- Model
-
-type Msg = Generate | LengthChange String
-
-
 type alias Model =
   { password : String
   , length : Int
   }
 
-
-model : Model
-model =
-  Model "" 0
-
-main =
-  beginnerProgram { model = model, view = view, update = update }
+init : (Model, Cmd Msg)
+init =
+  (Model "" 8, Cmd.none)
 
 
+
+-- ======================================================================
+-- Subscriptions
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+      
+-- ======================================================================
 -- View
-
 view model =
   div []
-    [ input [ placeholder "Number of characters", onInput LengthChange ] []
+    [ input [
+           placeholder "Number of characters"
+          , onInput LengthChange
+          ] []
     , button [ onClick Generate ] [ text "Generate" ]
     , text model.password
     ]
 
 
-generateRandomPassword = 
-  "ola"
 
+
+-- ======================================================================
 -- Update
-
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of 
     Generate ->
-      { model | password = generateRandomPassword }
+      ({model | password = ""}, Random.generate NewRandomChar (Random.Char.char 32 126))
     -- String.toInt can fail. It returns a Result so we convert to maybe and set a default 
-    LengthChange len -> 
-      { model | length = (String.toInt len |> Result.toMaybe |> Maybe.withDefault 8) }
+
+    LengthChange len ->
+        let
+            -- length is at least 1
+            newLength =
+                Basics.max
+                    (String.toInt len |> Result.toMaybe |> Maybe.withDefault 1)
+                    1
+        in
+            ({ model | length = newLength }, Cmd.none)
+
+    NewRandomChar char ->
+        let
+            log = Debug.log "NewRandomChar" char
+        in
+            (
+             {model | password = model.password ++ String.fromChar char},
+             if String.length model.password == (model.length - 1) then
+                 Cmd.none
+             else
+                 Random.generate NewRandomChar (Random.Char.ascii)
+            )
